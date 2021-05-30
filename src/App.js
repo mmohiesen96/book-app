8ea4e7362be5017f18d1af15e78b03e1cdc0c768
mmohiesen96 <mmohiesen996@gmail.com>
2021-05-30 16:33:29 +0300

    Warning

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './Header';
-import IsLoadingAndError from './IsLoadingAndError';
 import './App.css';
 import Footer from './Footer';
 import { withAuth0 } from '@auth0/auth0-react';
