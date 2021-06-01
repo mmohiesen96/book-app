1661d9deb5463336df51ae734f476013a8fd3012
mmohiesen96 <mmohiesen996@gmail.com>
2021-06-01 17:41:47 +0300

    lab13 Done

--- src/myFavoriteBooks.js
+++ src/myFavoriteBooks.js
@@ -4,18 +4,35 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import './myFavoriteBooks.css';
 import { withAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
-import Carousel from 'react-bootstrap/Carousel';
+import { Card, Button } from 'react-bootstrap';
+import AddBook from './AddBook';
 
 class MyFavoriteBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: [],
+      showModal:false,
+      bookName:'',
+      description:'',
+      imageUrl:''
     }
 
     console.log(this.state.books);
   }
 
+
+  handleModal = ()=> {
+    this.setState( {
+      showModal:true
+    })    
+  }
+
+  closeModal = ()=> {
+    this.setState( {
+      showModal:false
+    })    
+  }
   componentDidMount = async () => {
     const books = await axios.get('http://localhost:3001/books', { params: { email: this.props.auth0.user.email } })
     console.log('books', books.data)
@@ -26,33 +43,83 @@ class MyFavoriteBooks extends React.Component {
     console.log(this.state.books);
   }
 
+
+  setName = (e) => {
+    this.setState({
+      bookName:e.target.value
+    })
+  }
+
+  setDescription = (e) => {
+    this.setState({
+      description:e.target.value
+    })
+  }
+  setImage = (e) => {
+    this.setState({
+      imageUrl:e.target.value
+    })
+  }
+
+  addBook = async(e) => {
+    e.preventDefault();
+    
+    const bookData = {
+      name : this.state.bookName,
+      description : this.state.description,
+      image_url : this.state.imageUrl,
+      email : this.props.auth0.user.email,
+    }
+    let newBooks = await axios.post('http://localhost:3001/addBook', bookData);
+
+    this.setState({
+      books:newBooks.data,
+      showModal:false
+    })
+  }
+
+  deleteBook = async(index) => {
+    const userEmail = {
+      email:this.props.auth0.user.email
+    }
+    let newBooks = await axios.delete(`http://localhost:3001/deleteBook/${index}`,{params:userEmail})
+
+    this.setState({
+      books:newBooks.data
+    })
+  }
   render() {
     return (
       <>
+        <Button variant="primary" onClick={this.handleModal}>Add Book</Button>
+        {this.state.showModal && <AddBook hideModal={this.closeModal} showModalBool ={this.state.showModal} handleName={this.setName}
+          handleDescription={this.setDescription}
+          handleImage={this.setImage}
+          addBook = {this.addBook}
+        ></AddBook> }
         <Jumbotron>
           <h1>Book Collection</h1>
           <p>
             This is a collection of my favorite books
         </p>
-        <Carousel>
-          {this.state.books.map (item => {
-            return (
-              <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={item.image_url}
-              alt={item.name}
-            />
-            <Carousel.Caption>
-              <h3>{item.name}</h3>
-              <p>{item.description}</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-            )
-          })}
-          
-        </Carousel>
         </Jumbotron>
+
+        {
+          this.state.books.map((item,idx) => {
+            return (
+              <Card style={{ width: '18rem' }} key={idx}>
+                <Card.Img variant="top" src={item.image_url} />
+                <Card.Body>
+                  <Card.Title>{item.name}</Card.Title>
+                  <Card.Text>
+                    {item.description}
+                  </Card.Text>
+                  <Button variant="primary" onClick={() => this.deleteBook(idx)}>Delete</Button>
+                </Card.Body>
+              </Card>
+            )
+          })
+        }
       </>
     )
   }
